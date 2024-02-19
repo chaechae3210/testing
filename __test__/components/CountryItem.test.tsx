af8f6bb620391bc679b457cf5a3b09bc6d5c68ef
chaechae3210 <chaechae@naraspace.com>
2024-02-20 01:11:36 +0900

    test: CountryItem 테스트 코드 주석 추가

--- __test__/components/CountryItem.test.tsx
+++ __test__/components/CountryItem.test.tsx
@@ -16,6 +16,7 @@ const mockProps = {
 };
 
 describe("CountryItem Component", () => {
+  // 컴포넌트 ui 테스트
   it("CountryItem에 프롭스를 넘겼을 때 국가 정보가 올바르게 렌더링 됨.", () => {
     render(<CountryItem {...mockProps} />);
 
@@ -25,12 +26,14 @@ describe("CountryItem Component", () => {
     expect(screen.getByText("인구 : 51780579")).toBeInTheDocument();
   });
 
-  // 위의 테스트를 스냅샷 테스트로 바꾸었을 때
+  // 스냅샷 테스트 : 컴포넌트의 ui 변화를 테스트할 때 유용
+  // 스토리북을 쓴다는 가정이면 스토리북을 통해 ui 테스트를 하는 것이 더 합리적인 것 같습니다.
   it("CountryItem 스냅샷 테스트", () => {
     const { container } = render(<CountryItem {...mockProps} />);
     expect(container).toMatchSnapshot();
   });
 
+  // li 클릭시 push가 호출되는지에 대한 기능 단위테스트
   it("li를 클릭하면 국가 정보 디테일 페이지로 push됨", async () => {
     mockRouter.push("/");
 
@@ -39,9 +42,5 @@ describe("CountryItem Component", () => {
     await userEvent.click(container);
 
     expect(mockRouter.asPath).toEqual(`/country/${mockProps.code}`);
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
   });
 });
